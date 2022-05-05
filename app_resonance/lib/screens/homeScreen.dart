5d4ef3fb41d9faa9a7db4234e4d94606bb5883b8
sagender <44262173@certus.edu.pe>
2022-05-05 01:54:10 -0500

    Imagen principal modificaciones

--- app_resonance/lib/screens/homeScreen.dart
+++ app_resonance/lib/screens/homeScreen.dart
@@ -1,8 +1,8 @@
-// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, duplicate_ignore, camel_case_types, prefer_typing_uninitialized_variables, non_constant_identifier_names
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, duplicate_ignore, camel_case_types, prefer_typing_uninitialized_variables, non_constant_identifier_names, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
-import '../widget/category_card.dart';
+import '../constants.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -78,7 +78,7 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
-            height: size.height * .40, //.45,
+            height: size.height * .45, //.45,
             // ignore: prefer_const_constructors
             decoration: BoxDecoration(
               color: Colors.amberAccent, //Color(0xFFF5CEB8),
@@ -122,6 +122,20 @@ class HomeScreen extends StatelessWidget {
                       border: InputBorder.none,
                     )),
                   ),
+                  ListHorizontal(
+                      'Ciudad de Lima',
+                      Row(
+                        children: [
+                          Image.network(
+                            'https://p4.wallpaperbetter.com/wallpaper/317/319/594/neon-genesis-evangelion-zero-two-darling-in-the-franxx-crossover-wallpaper-preview.jpg',
+                            width: 550.0,
+                            fit: BoxFit.cover,
+                          ),
+                          SizedBox(width: 10.0)
+                        ],
+                      ),
+                      1)
+                  /*,
                   Expanded(
                     child: GridView.count(
                       //He modificado esto parámetros
@@ -151,13 +165,60 @@ class HomeScreen extends StatelessWidget {
                         )
                       ],
                     ),
-                  ),
+                  ),*/
                 ],
               ),
             ),
           )
         ],
       ),
+    );
+  }
+
+  Column ListHorizontal(String titulo, Widget item, int cantidad) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 10.0),
+          child: Text(
+            titulo,
+            style: TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0),
+          ),
+        ),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(13),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(13),
+                // ignore: prefer_const_literals_to_create_immutables
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 17),
+                    blurRadius: 17,
+                    spreadRadius: -23,
+                    color: KShadowColor,
+                  )
+                ])
+            //formato de la imagen
+            ,
+            height: 300.0,
+            child: ListView.builder(
+              //ListView
+              scrollDirection: Axis.horizontal,
+              itemCount: 1,
+              //children: <Widget>[ ItemRedondeado(),ItemRedondeado1(),ItemRedondeado2(), ItemRedondeado(), ItemRedondeado1(), ],
+              itemBuilder: (context, index) {
+                return item;
+              },
+            ),
+          ),
+        )
+      ],
     );
   }
 }
