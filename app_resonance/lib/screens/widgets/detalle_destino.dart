93448d1bb5e2d733d48492574561f1d6f3894142
hugo <77203387@certus.edu.pe>
2022-06-16 22:13:27 -0500

    adregue y corregi el widgte de destino

--- app_resonance/lib/screens/widgets/detalle_destino.dart
+++ app_resonance/lib/screens/widgets/detalle_destino.dart
@@ -3,15 +3,21 @@
 import 'package:flutter/material.dart';
 import 'package:appcarrusel/screens/widgets/background_destino.dart';
 
-class DetalleGradienteScreen extends StatelessWidget {
-  const DetalleGradienteScreen({Key? key}) : super(key: key);
+class DetalleDestinocreen extends StatelessWidget {
+  const DetalleDestinocreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
           // ignore: prefer_const_constructors
-          children: [Background(), SafeArea(child: _Detalle()), _Regresar()]),
+          children: [
+            Background(),
+            SafeArea(child: _Detalle()),
+            SafeArea(child: _Detalle1()),
+            _Regresar(),
+            _Regresar1()
+          ]),
     );
   }
 }
@@ -31,7 +37,7 @@ class _Detalle extends StatelessWidget {
         margin: EdgeInsets.only(left: 20, top: 5.8, right: 20.0),
         child: Column(children: const [
           Hero(
-            tag: 'Ventana',
+            tag: '1',
             child: CircleAvatar(
               //esto es para agrandar el radius
 
@@ -53,6 +59,65 @@ class _Detalle extends StatelessWidget {
 
 class _Regresar extends StatelessWidget {
   const _Regresar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 30,
+      left: 15,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Icon(
+          Icons.arrow_back_ios,
+          size: 25,
+          color: Colors.pink,
+        ),
+      ),
+    );
+  }
+}
+
+class _Detalle1 extends StatelessWidget {
+  const _Detalle1({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 60,
+        ),
+        margin: EdgeInsets.only(left: 20, top: 5.8, right: 20.0),
+        child: Column(children: const [
+          Hero(
+            tag: '2',
+            child: CircleAvatar(
+              //esto es para agrandar el radius
+
+              maxRadius: 90,
+              backgroundImage: NetworkImage(
+                  'https://elcomercio.pe/resizer/U1kW-7m-EKja9pw3ZFtNpZ89InM=/1200x800/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/II7LIHJYTVB5VHYABREBVGSPEI.jpg'),
+              //child: ClipOval(),
+            ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Text(
+              'Mi razóneu et esse ex sit cillum. Est duis excepteur et cillum fugiat voluptate tempor laboris sint proident dolore reprehenderit. Ut duis non exercitation duis sint est reprehenderit eiusmod. Deserunt veniam quis ea excepteur eiusmod incididunt officia veniam enim nisi ipsum laboris ex pariatur. Pariatur veniam ex aliquip minim. Reprehenderit consectetur in nostrud non ad aliquip. Consectetur aliqua dolor quis anim veniam deserunt laboris fugiat cupidatat minim.',
+              style: TextStyle(color: Colors.black54))
+        ]));
+  }
+}
+
+class _Regresar1 extends StatelessWidget {
+  const _Regresar1({
     Key? key,
   }) : super(key: key);
 
