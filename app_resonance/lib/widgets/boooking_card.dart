343c66f43709c229fe5512aae37969b26b412813
sagender <44262173@certus.edu.pe>
2022-06-17 01:37:38 -0500

    Implementación del meodelo

--- app_resonance/lib/widgets/boooking_card.dart
+++ app_resonance/lib/widgets/boooking_card.dart
@@ -1,15 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BookingCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Container(
           margin: EdgeInsets.only(top: 30, bottom: 50),
           width: double.infinity,
           height: 400,
-          color: Colors.red),
+          decoration: _cardBorders(),
+          child: Stack(
+            alignment: Alignment.bottomLeft,
+            children: [
+              Backgroungimage(),
+              _ProductDetails(),
+              Positioned(top: 0, child: _PriceTag())
+            ],
+          ),
+        ));
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+          color: Colors.white70,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, offset: Offset(0, 7), blurRadius: 10)
+          ]);
+}
+
+class _PriceTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "S/120",
+        style: GoogleFonts.indieFlower(fontSize: 20, color: Colors.black87),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(25), bottomLeft: Radius.circular(25)),
+      ),
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 0),
+      child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          width: double.infinity,
+          height: 70,
+          //color: Colors.red,
+          decoration: _buildBoxDecoration(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Barranco",
+                style: GoogleFonts.indieFlower(
+                    fontSize: 20,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.bold),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                "ID del producto",
+                style: GoogleFonts.indieFlower(
+                  fontSize: 10,
+                  color: Colors.black87,
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
+}
+
+class Backgroungimage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage("assets/images/jar-loading.gif"),
+          image: NetworkImage("https://via.placeholder.com/400x300/f6f6f6"),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
